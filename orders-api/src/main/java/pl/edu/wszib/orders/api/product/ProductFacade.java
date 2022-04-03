f58762361812143ea2d0554afee4c6bc55edc0f6
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2022-04-03 20:30:49 +0200

    extract modules

--- orders-api/src/main/java/pl/edu/wszib/orders/api/product/ProductFacade.java
+++ orders-api/src/main/java/pl/edu/wszib/orders/api/product/ProductFacade.java
@@ -3,7 +3,7 @@ package pl.edu.wszib.orders.api.product;
 import java.util.Optional;
 
 public interface ProductFacade {
-    ProductApi create(final ProductApi product);
+    ProductApi create(ProductApi product);
 
     Optional<ProductApi> findById(String id);
 }
