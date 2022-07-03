39eba362e26090f59f03baafb9a60321e7b29059
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2022-07-03 20:57:12 +0200

    add console-ui module, design console menu

---
+++ orders-console-ui/src/main/java/pl/edu/wszib/orders/consoleui/ConsoleUI.java
@@ -0,0 +1,32 @@
+package pl.edu.wszib.orders.consoleui;
+
+public class ConsoleUI {
+
+    public void run() {
+        // Główne menu:
+        // 1. Utwórz zamówienie
+        // 2. Wyszukaj zamówienie
+        // 3. Dodaj produkt do zamówienia
+        // 4. Usuń produkt z zamówienia
+        // 0. Wyjście
+
+        // 1. Identyfikator utworzonego zamówienia: ${orderId}
+
+        // 2.1 Podaj identyfikator zamówienia: ${orderId}
+        // 2.2 (odczytaj orderId)
+        // 2.3 Znaleziono zamówienie: ${orderId}
+        // 2.4 Wystąpił błąd: ${error}
+        // 2.5 (powrót do głównego menu)
+
+        // 3.1 Podaj identyfikator zamówienia: ${orderId}
+        // 3.2 (odczytaj orderId)
+        // 3.3 Wyświetl listę dostępnych produktów: ${allProducts}
+        // 3.4 Podaj identyfikator produktu, który chcesz dodać do zamówienia: ${productId}
+        // 3.5 Wyświetl zmodyfikowane zamówienie: ${order}
+        // 3.6 Wystąpił błąd: ${error}
+        // 3.7 (powrót do głównego menu)
+
+        // 4. (na podstawie punktu 3)
+        // 5. (Przerwanie pętli głównej programu)
+    }
+}
--- orders-infrastructure/src/main/java/pl/edu/wszib/orders/OrdersApplication.java
+++ orders-infrastructure/src/main/java/pl/edu/wszib/orders/OrdersApplication.java
@@ -2,6 +2,7 @@ package pl.edu.wszib.orders;
 
 import pl.edu.wszib.orders.api.product.ProductApi;
 import pl.edu.wszib.orders.application.product.ProductFacade;
+import pl.edu.wszib.orders.consoleui.ConsoleUI;
 import pl.edu.wszib.orders.infrastructure.product.InMemoryProductRepository;
 
 import java.math.BigDecimal;
@@ -10,10 +11,11 @@ import java.util.Optional;
 public class OrdersApplication {
 
     public static void main(final String[] args) {
-        final ProductApi product = new ProductApi("test id", "test name", BigDecimal.TEN);
-        final ProductFacade productFacade = new ProductFacade(new InMemoryProductRepository());
-        productFacade.create(product);
-        final Optional<ProductApi> foundProduct = productFacade.findById(product.id());
-        System.out.println("Found product: " + foundProduct);
+        new ConsoleUI().run();
+//        final ProductApi product = new ProductApi("test id", "test name", BigDecimal.TEN);
+//        final ProductFacade productFacade = new ProductFacade(new InMemoryProductRepository());
+//        productFacade.create(product);
+//        final Optional<ProductApi> foundProduct = productFacade.findById(product.id());
+//        System.out.println("Found product: " + foundProduct);
     }
 }
